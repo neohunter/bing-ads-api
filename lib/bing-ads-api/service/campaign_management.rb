2705160791a42c64373d3b354c2bb5ce9f1af403
Michael Curtis <m.curtis@me.com>
2016-04-26 14:33:38 -0700

    adding get_campaigns_by_ids

--- lib/bing-ads-api/service/campaign_management.rb
+++ lib/bing-ads-api/service/campaign_management.rb
@@ -69,6 +69,19 @@ module BingAdsApi
 			end
 			return campaigns
 		end
+    
+		def get_campaigns_by_ids(account_id, campaign_ids)
+			response = call(:get_campaigns_by_ids, 
+				{account_id: account_id, 
+         campaign_ids: { 'ins1:long' => campaign_ids } })
+			response_hash = get_response_hash(response, __method__)
+			campaigns_hash = response_hash[:campaigns][:campaign]
+      campaigns_hash = [ campaigns_hash ] if campaigns_hash.is_a? Hash
+      campaigns = campaigns_hash.map do |camp_hash|
+				BingAdsApi::Campaign.new(camp_hash)
+			end
+			return campaigns
+		end
 
     def get_keywords_by_ad_group_id(ad_group_id)
       response = call(:get_keywords_by_ad_group_id, {ad_group_id: ad_group_id})
